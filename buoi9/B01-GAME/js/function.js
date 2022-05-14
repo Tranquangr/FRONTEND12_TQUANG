391ae2c547c8989c27a540b20cb3bcce6faab85f
unknown <quangt300497@gmail.com>
2022-05-14 16:33:38 +0700

    update GAME

--- buoi9/B01-GAME/js/function.js
+++ buoi9/B01-GAME/js/function.js
@@ -7,11 +7,11 @@ $(document).ready(function(){
 //Lấy dữ liệu người dùng chọn mức độ show ra số lượng các ô tương ứng
 function numberCells(obj)
 {
+    $("#funcHelp").show();
     num = -1;
     for (let i = 0 ; i<numberSelected.length;i++) {
         numberSelected[i].innerHTML= "";
     }
-    boxRecommend.innerHTML="";
     areaHandle.innerHTML="";
     let value = obj.value;
     if(value == 1)
@@ -66,9 +66,37 @@ function numberCells(obj)
                 if (boxHandle[i].getAttribute('value') == randomNumber) {
                     boxHandle[i].style.backgroundColor = "green";
                     boxHandle[i].style.color = "white";
-                    numberSelected[num].innerHTML = randomNumber;
-                    choice = confirm("Bạn đã chiến thắng, Chơi lại?")
-                    location.reload();
+                    let rec = createRec();
+                    let text = "Bạn đã chọn chính xác";
+                    boxRecommend.appendChild(rec);
+                    $("h3").last().text("ĐÚNG RỒI");
+                    $("p").last().text(text);
+                    let result = createResult();
+                    result.innerHTML = "Số bí mật là:" + randomNumber;
+                    nameRank = prompt("Bạn muốn lưu danh sử sách thì điền tiên nhé !!!");
+                    
+                    if (value == 1)
+                    {
+                            scoreRank = 100 - num*10;
+                    }else if (value == 2)
+                    {
+                            scoreRank = 200 - num*10;
+                    }else{
+                            scoreRank = 300- num*10;
+                    }
+                    
+                    var obj = {
+                        name : nameRank,
+                        score : scoreRank,
+                    }
+                    boxRecommend.appendChild(result);
+                    list.push(obj);
+                    render(list);
+                    save_json(list);
+                    setTimeout(function(){
+                        choice = confirm("Bạn đã chiến thắng, Chơi lại?")
+                        location.reload();
+                    },100);
                 }
                 else
                 {
@@ -90,11 +118,71 @@ function numberCells(obj)
                     document.querySelectorAll(".boxRecommend .card-body p")[num].innerText =(num+1) + ". Lần " + (num+1) + ": " + boxHandle[i].getAttribute('value') + text ;
                     
                 }
-            }else
-            {
-                choice = confirm("Bạn đã thua, Chơi lại?")
-                location.reload();
             }
+            if(num==2)
+            {
+                if (boxHandle[i].getAttribute('value') == randomNumber) {
+                    boxHandle[i].style.backgroundColor = "green";
+                    boxHandle[i].style.color = "white";
+                    let rec = createRec();
+                    let text = "Bạn đã chọn chính xác";
+                    boxRecommend.appendChild(rec);
+                    $("h3").last().text("ĐÚNG RỒI");
+                    $("p").last().text(text);
+                    let result = createResult();
+                    result.innerHTML = "Số bí mật là:" + randomNumber;
+                    boxRecommend.appendChild(result);
+                    nameRank = prompt("Bạn muốn lưu danh sử sách thì điền tiên nhé !!!");
+                    if (value == 1)
+                    {
+                            scoreRank = 100 - num*10;
+                    }else if (value == 2)
+                    {
+                            scoreRank = 200 - num*10;
+                    }else{
+                            scoreRank = 300- num*10;
+                    }
+                    var obj = {
+                        name : nameRank,
+                        score : scoreRank,
+                    }
+                    list.push(obj);
+                    render(list);
+                    save_json(list);
+                    setTimeout(function(){
+                        choice = confirm("Bạn đã chiến thắng, Chơi lại?")
+                        location.reload();
+                    },100);}
+                    else
+                    {
+                        boxHandle[i].style.backgroundColor = "red";
+                        boxHandle[i].style.color = "white";
+                        numberSelected[num].innerHTML = boxHandle[i].getAttribute('value');
+                        let rec = createRec();
+                        let text = "";
+                        if (boxHandle[i].getAttribute('value') < randomNumber)
+                        {
+                            text = " Số của bạn nhỏ hơn số bí mật";
+                        }
+                        else
+                        {
+                            text = " Số của bạn lớn hơn số bí mật"
+                        }
+                        
+                        boxRecommend.appendChild(rec);
+                        document.querySelectorAll(".boxRecommend .card-body p")[num].innerText =(num+1) + ". Lần " + (num+1) + ": " + boxHandle[i].getAttribute('value') + text ;
+
+                        let result = createResult();
+                        result.innerHTML = "Số bí mật là:" + randomNumber;
+                        boxRecommend.appendChild(result);
+                        setTimeout(function(){
+                            choice = confirm("Bạn đã thua, Chơi lại?")
+                            location.reload();
+                        },500)
+                        
+                    }
+            }
+            boxNumberSelected.style.display ="flex";    
            
         })
     }
@@ -103,6 +191,63 @@ function numberCells(obj)
 
 }
 
+function help1(){
+    if (select_level.value ==1)
+    {
+        const box =document.querySelectorAll(".box");
+        let i = box.length;
+        while (i>6)
+        {
+            let randomBox = Math.round(Math.random() * 24);
+            console.log(box[randomBox].getAttribute("value"));
+            if (box[randomBox].getAttribute("value") != randomNumber && randomBox > 2)
+            {
+                box[randomBox].style.display = "none";
+                i--;
+            }
+        }
+    }else if (select_level.value ==2)
+    {
+        const box =document.querySelectorAll(".box");
+        let i = box.length;
+        while (i>6)
+        {
+            let randomBox = Math.round(Math.random() * 30);
+            console.log(box[randomBox].getAttribute("value"));
+            if (box[randomBox].getAttribute("value") != randomNumber && randomBox > 2)
+            {
+                box[randomBox].style.display = "none";
+                i--;
+            }
+        }
+    }
+    else{
+        const box =document.querySelectorAll(".box");
+        let i = box.length;
+        while (i>6)
+        {
+            let randomBox = Math.round(Math.random() * 36);
+            console.log(box[randomBox].getAttribute("value"));
+            if (box[randomBox].getAttribute("value") != randomNumber && randomBox > 2)
+            {
+                box[randomBox].style.display = "none";
+                i--;
+            }
+        }
+    }
+}
+
+function help2(){
+    let randomBox = Math.round(Math.random() * 24);
+    alert("gOOgle mách bẢO bạn lÀ: " + "---->  " +randomBox +"   <-----");
+}
+
+function help3(){
+    b = b + 5;
+    console.log(b);
+    $(".box").css("fontSize", b);
+    
+}
 
 //Hàm tạo box
 function createBox(){
@@ -110,6 +255,9 @@ function createBox(){
     box.classList.add("box");
     box.classList.add("me-2");
     box.classList.add("my-2");
+    box.classList.add("animate__animated");
+    box.classList.add("animate__tada");
+    box.classList.add("animate__slow");
     return box;
 }
 
@@ -121,4 +269,45 @@ function createRec(){
     box.appendChild(title);
     box.appendChild(desc);
     return box;
+}
+
+function createResult(){
+    let box = document.createElement("div");
+    return box;
+}
+
+function showRanK(){
+    document.querySelector(".modal_rank").style.display = "flex";
+}
+function hideRank(){
+    document.querySelector(".modal_rank").style.display = "none";
+}
+
+function render(list){
+    var html= "";
+    for(var i = 0 ; i< list.length; i++){
+        html+='<tr>';
+        html+='<td>'+ list[i].name +'</td>';
+        html+='<td>'+ list[i].score +'</td>';
+        html+='</tr>';
+    }
+    result.innerHTML=html
+}
+
+function getJSON(){
+    if (localStorage.getItem("list") === null) {
+        localStorage.setItem('list',[]);
+      }
+    else
+    {
+        list = JSON.parse(localStorage.getItem('list'));
+        list.sort((a,b) => (a.score < b.score) ? 1 : ((b.score < a.score) ? -1 : 0));
+        console.log(list);
+        render(list);
+    }
+}
+
+function save_json(list){
+    var jsonString = JSON.stringify(list);
+    localStorage.setItem('list',jsonString);
 }
